936db0e7a3fb16d8343b5c8165869af6b72ff982
unknown <brayan.gonzalez23@correo.tdea.edu.co>
2026-02-24 17:32:08 -0500

    comentario

---
+++ clinic2/src/test/java/clinic2/clinic2/Clinic2ApplicationTests.java
@@ -0,0 +1,13 @@
+package clinic2.clinic2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clinic2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
